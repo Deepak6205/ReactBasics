ae04f9c145ad5b48647d7a8d95ab611a1c15333c
deepak kumar <deepak7292832956@gmail.com>
2024-08-25 20:13:10 +0530

    seperated card component

---
+++ src/component/Card.js
@@ -0,0 +1,27 @@
+import { useNavigate } from "react-router-dom";
+
+export const Card = ({ api }) => {
+  const navigate = useNavigate();
+  return (
+    <div className="cardParent">
+      {api.map((data) => {
+        return (
+          <div className="child" key={data.id}>
+            <img alt="abc" src={data.image} />
+            <h3>{data.title.slice(0, 25)}</h3>
+            <p>{data.price} rs.</p>
+            <p>{data.description.slice(0, 50)}</p>
+            <p>Rating = {data.rating.rate}</p>
+            <button
+              onClick={() => {
+                navigate(`HomeworkDetails/${data.id}`);
+              }}
+            >
+              Add to Cart
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
--- src/component/Homework.js
+++ src/component/Homework.js
@@ -1,98 +1,91 @@
-import React, { useEffect, useState } from 'react'
-import { Navbar } from './Navbar';
-import { Shimmer } from './Shimmer';
-import { useNavigate } from 'react-router';
-import { SelectMenu } from './SelectMenu';
+import React, { useEffect, useState } from "react";
+import { Navbar } from "./Navbar";
+import { Shimmer } from "./Shimmer";
+import { SelectMenu } from "./SelectMenu";
+import { Card } from "./Card";
 
 export const Homework = () => {
-    const[api,setApi] = useState([]);
-    const [loader,setLoader] = useState(false);
-    const[select,setSelect] = useState("");
-    const[filtervalue,setFiltervalue] = useState("");
-    const navigate = useNavigate();
-    const fetching = async () =>{
-        setLoader(true);
-        try{
-            const getApiData = await fetch(`https://fakestoreapi.com/products?sort=${filtervalue}`);
-            const getData = await getApiData.json();
-            setApi(getData);
-            console.log(getData);
-
-        }catch(error){
-            console.log(error);
-        }finally{setLoader(false)}
-    };
-    const categoryFetching = async () =>{
-        setLoader(true);
-        try{
-            const getApiData = await fetch(`https://fakestoreapi.com/products/category/${select}`);
-            const getData = await getApiData.json();
-            setApi(getData);
-            console.log(getData);
-
-        }catch(error){
-            console.log(error);
-        }finally{setLoader(false)}
-    };
-    const handleFilter = (action)=>{
-        setFiltervalue(action);
-        // if(action === 'asc'){
-            
-        //     setFiltervalue(action);
-        // }else{
-        //     setFiltervalue(action);
-        // }
+  const [api, setApi] = useState([]);
+  const [loader, setLoader] = useState(false);
+  const [select, setSelect] = useState("");
+  const [filtervalue, setFiltervalue] = useState("");
+  const fetching = async () => {
+    setLoader(true);
+    try {
+      const getApiData = await fetch(
+        `https://fakestoreapi.com/products?sort=${filtervalue}`
+      );
+      const getData = await getApiData.json();
+      setApi(getData);
+      console.log(getData);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoader(false);
     }
-    useEffect(()=>{
-        !select ? fetching() : categoryFetching()
-    },[filtervalue,select]);
-    if(loader){
-       return <Shimmer/>       
+  };
+  const categoryFetching = async () => {
+    setLoader(true);
+    try {
+      const getApiData = await fetch(
+        `https://fakestoreapi.com/products/category/${select}`
+      );
+      const getData = await getApiData.json();
+      setApi(getData);
+      console.log(getData);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoader(false);
     }
+  };
+  const handleFilter = (action) => {
+    setFiltervalue(action);
+    // if(action === 'asc'){
+
+    //     setFiltervalue(action);
+    // }else{
+    //     setFiltervalue(action);
+    // }
+  };
+  useEffect(() => {
+    !select ? fetching() : categoryFetching();
+  }, [filtervalue, select]);
+  if (loader) {
+    return <Shimmer />;
+  }
   return (
-    <div className='Parent'>
+    <div className="Parent">
+      <div>
+        <Navbar />
+      </div>
+      <div className="center">
+        <div></div>
         <div>
-            <Navbar/>
-            
+          <div className="flex-center">
+            <input type="search" placeholder="Search"></input>
+            <img
+              alt="g"
+              src="https://cdn0.iconfinder.com/data/icons/art-designing-glyph/2048/1871_-_Magnifier-512.png"
+            />
+          </div>
         </div>
-        <div className="center">
-            <div></div>
-            <div>
-                <div className='flex-center'>
-                    <input type="search" placeholder="Search"></input>
-                    <img alt='g' src='https://cdn0.iconfinder.com/data/icons/art-designing-glyph/2048/1871_-_Magnifier-512.png'/>
-                </div>
-            </div>
-            
-            <div className='sideBtn'>
-                <SelectMenu api={api} setSelect = {setSelect}/>
-                <button className='btn'onClick={()=>handleFilter('asc')}>Ascending</button>
-                <button className='btn' onClick={()=>handleFilter('desc')}>Descending</button>    
-            </div>
-            
-            
+
+        <div className="sideBtn">
+          <SelectMenu setSelect={setSelect} />
+          <button className="btn" onClick={() => handleFilter("asc")}>
+            Ascending
+          </button>
+          <button className="btn" onClick={() => handleFilter("desc")}>
+            Descending
+          </button>
         </div>
-            {/* <div className='sideBtn'>
+      </div>
+      {/* <div className='sideBtn'>
                 <button className='btn'onClick={()=>handleFilter('asc')}>Ascending</button>
                 <button className='btn' onClick={()=>handleFilter('desc')}>Descending</button>
             </div> */}
-    <div className='cardParent'>
-        {api.map((data) => {
-            return(
-        <div className='child' key={data.id}>
-            <img alt='abc' src={data.image}/>
-            <h3>{data.title.slice(0,25)}</h3>
-            <p>{data.price} rs.</p>
-            <p>{data.description.slice(0,50)}</p>
-            <p>Rating = {data.rating.rate}</p>
-            <button onClick={()=>{
-                navigate(`HomeworkDetails/${data.id}`)
-            }}>Add to Cart</button>
-        </div> 
-            )
-        })}
-        
+      <Card api={api} />
     </div>
-    </div>
-  )
-}
+  );
+};
